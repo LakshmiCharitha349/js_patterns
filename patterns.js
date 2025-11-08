1a20907bbaae69789e91c03d3ffefff818ed619f
Marthala Charitha <marthalacharitha@Marthalas-MacBook-Pro.local>
2025-11-08 08:20:36 +0530

    add filled rectangle function

--- patterns.js
+++ patterns.js
@@ -1,1 +1,31 @@
+
 console.log("--- Generate patterns ---");
+
+const filled = (char, size) => char.repeat(size);
+const hollowLine = size => `*${filled(" ", size - 2)}*`;
+
+const repeatRC = (rs, cs) => {
+  const array = [];
+  for (let row = 1; row <= rs; row++) {
+    array.push(cs);
+  }
+
+  return array;
+}
+
+const filledRectangle = input => {
+  return input.map(x => filled("*", x));
+}
+
+function applyStyles(style, rows, clms) {
+  return style(repeatRC(rows, clms));
+}
+
+function generatePatterns() {
+
+  console.log(applyStyles(filledRectangle, 5, 3));
+  console.log(applyStyles(filledRectangle, 1, 2));
+}
+
+
+generatePatterns();
