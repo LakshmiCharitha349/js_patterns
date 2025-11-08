92f8a03888874a8afc91186ed2b58baa4b142625
Marthala Charitha <marthalacharitha@Marthalas-MacBook-Pro.local>
2025-11-08 21:29:09 +0530

    add isnot duplicate function

--- practice_mfr.js
+++ practice_mfr.js
@@ -1,14 +1,22 @@
-console.log("--- Usage of Map Filter Reduce Fns --- ");
-
 //1. Festival ribbon count
 
 const countOf = function (count, string) {
   return string === "blue" ? count + 1 : count;
 }
 
-const reduceData = (reducer, data, intialValue = 0) => data.reduce(reducer, intialValue);
+//3. birdwatchimg duplicate removal
 
+const isNotDuplicate = function (bird,index,list) {
+    if(list.indexOf(bird) === index) {
+      return true;
+    }
+    
+  return false;
+} 
 
+// const data = [["Orion", "Leo"],
+// ["Taurus"],
+// ["Orion", "Gemini"]]
 function isEqual(array1, array2) {
   if (!Array.isArray(array1)) {
     return array1 === array2;
@@ -23,13 +31,13 @@ function isEqual(array1, array2) {
   return true;
 }
 
-function displayMessage(data, result, expected, gist) {
+function displayMessage(result, expected, gist) {
   const isPassed = isEqual(result, expected);
   const resultSymbol = isPassed ? "✅" : "❌";
   let message = resultSymbol + gist;
 
   if (!isPassed) {
-    const inputFrag = `\ninput: ${data}\n`;
+    //const inputFrag = `\ninput: ${data}\n`;
     const actualFragment = `Result: ${result}\n`;
     const expectedFragment = `Expected: ${expected}`;
     message += inputFrag + actualFragment + expectedFragment;
@@ -38,18 +46,29 @@ function displayMessage(data, result, expected, gist) {
   console.log(message);
 }
 
-function testOperations(type, operation, data, expected, gist) {
-  const result = type(operation, data);
-
-  displayMessage(data, result, expected, gist);
+function testOperations( actual, expected, gist) {
+  displayMessage( actual, expected, gist);
 }
 
 
+function testsOfRibbonCount() {
+  console.log("\n--- RIBBON COUNT ---\n");
 
-function tests() {
-
-  testOperations(reduceData, countOf, ["red", "blue", "red", "green", "red", "blue"], 2, "an array");
+  testOperations(["red", "blue", "red", "green", "red", "blue"].reduce(countOf,0),2, "an array");
+  testOperations(["red","red", "green", "red"].reduce(countOf,0),0, "an array");
 }
 
-tests();
+function testsOfRemoveDuplicate() {
+  console.log("\n--- REMOVE DUPLICATE ---\n");
+
+  testOperations(["sparrow","crow","parrot","crow"].filter(isNotDuplicate),
+  ["sparrow","crow","parrot"],"list of birds")
+}
+
+function testAll() {
+  testsOfRibbonCount();
+  testsOfRemoveDuplicate();
+}
+
+testAll();
 
