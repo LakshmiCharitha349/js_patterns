7038bc966495869ebb4418f7a147872870934a32
Marthala Charitha <marthalacharitha@Marthalas-MacBook-Pro.local>
2025-11-08 09:12:52 +0530

    add hollowRectangle function

--- patterns.js
+++ patterns.js
@@ -1,8 +1,7 @@
 
-console.log("--- Generate patterns ---");
-
 const filled = (char, size) => char.repeat(size);
 const hollowLine = size => `*${filled(" ", size - 2)}*`;
+const underLine = x => filled("-", x);
 
 const repeatRC = (rs, cs) => {
   const array = [];
@@ -13,19 +12,38 @@ const repeatRC = (rs, cs) => {
   return array;
 }
 
-const filledRectangle = input => {
-  return input.map(x => filled("*", x));
+const repeatInput = (rs, cs) => {
+  const array = [];
+  array.push(cs);
+  array.push(repeatRC(rs - 2, cs));
+  array.push(cs);
+  return array;
 }
 
-function applyStyles(style, rows, clms) {
-  return style(repeatRC(rows, clms));
+const hollowRectangle = size => {
+  const pattern = [];
+  pattern.push(filled("*", size[0]));
+  pattern.push(size[1].map(x => hollowLine(x)));
+  pattern.push(filled("*", size[2]));
+
+  return pattern.flat();
+}
+
+const filledRectangle = size => {
+  return size.map(x => filled("*", x));
+}
+
+function applyStyles(style, size) {
+  return style(size).join("\n");
 }
 
 function generatePatterns() {
+  console.log("--- Generate patterns ---");
 
-  console.log(applyStyles(filledRectangle, 5, 3));
-  console.log(applyStyles(filledRectangle, 1, 2));
+  console.log(applyStyles(filledRectangle, repeatRC(5, 3)), "\n");
+  console.log(applyStyles(filledRectangle, repeatRC(1, 2)), "\n");
+  console.log(applyStyles(hollowRectangle, repeatInput(4, 4)), "\n");
+  console.log(applyStyles(hollowRectangle, repeatInput(2, 2)), "\n");
 }
-
 
 generatePatterns();
