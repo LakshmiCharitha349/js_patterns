959b3d82b0ba0a970cfffbdc6514359f2baf96f8
Marthala Charitha <marthalacharitha@Marthalas-MacBook-Pro.local>
2025-11-09 18:04:40 +0530

    add testcases

--- practice_mfr.js
+++ practice_mfr.js
@@ -34,12 +34,9 @@ const count = function (count, number) {
 
 const isBelow32 = (weather) => weather < 32;
 
-const isUnique = (color,index,list) => { 
-  if(list.indexOf(color) === list.lastIndexOf(color)) {  
-    return true;
-  }
-  return false;
-};
+const isUnique = (color,index,list) => list.indexOf(color) === list.lastIndexOf(color);
+
+const isPositive = (number) => number > 0;
 
 function isEqual(array1, array2) {
   if (!Array.isArray(array1)) {
@@ -74,83 +71,66 @@ function testOperations(result, expected, gist) {
   displayMessage(result, expected, gist);
 }
 
-
-function testsOfRibbonCount() {
-  console.log("\n--- RIBBON COUNT ---\n");
-
-  testOperations(countOf(["red", "blue", "red", "green", "red", "blue"],"blue",0),2, "an array");
-  testOperations(countOf(["red", "red", "green", "red"],"blue", 0),0, "an array");
-}
-
-function testsStargazingLog() {
-  console.log("\n--- STARGAZING LOG---\n");
-
-  testOperations(filter(isNotDuplicate,[["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]]),
-    ["Orion", "Leo", "Taurus", "Gemini"], "list of names");
-}
-
-
-function testsOfRemoveDuplicate() {
-  console.log("\n--- REMOVE DUPLICATE BIRDS ---\n");
-
-  testOperations(filter(isNotDuplicate,["sparrow", "crow", "parrot", "crow"]),
-    ["sparrow", "crow", "parrot"], "list of birds"
+function testsOfEvery() {
+  testOperations(every(isBelow32,[[22, 23],[25, 24, 22],[29]]),
+    true, "listof temperaues"
+  );
+  testOperations(every(isPositive,[[2,5],[3,2,1],[6]]),
+    true, "listof numbers"
   );
 
+  
 }
 
-function testsOfAttendenceCheck() {
-  console.log("\n--- ATTENDENCE CHECK ---\n");
-
-  testOperations(filter(isNotDuplicate,[["nani", "neel", "ravi", "asha"], ["neel", "meera"], ["neel"]]),
-    ["nani", "neel", "ravi", "asha", "meera"], "list of students"
+function testsOfSome() {
+  testOperations(some([["step","tap"], ["turn","step"]],"turn"),true, "listof music notes");
+  testOperations(some([["mi", "fa", "so"], ["do", "mi"], ["fa"]],"so"),true, "listof music notes"
   );
-
-}
-
-function testsOfCandyCount() {
-  console.log("\n--- CANDY COUNT ---\n");
-
-  testOperations(reduce(count,[[5, 3], [1, 3, 2], [3, 2]],0,),19, "listof candies added");
-
-}
-
-function testsOfMusicNotes() {
-  console.log("\n--- MUSIC NOTES OF DO ---\n");
-
   testOperations(some([["mi", "fa", "so"], ["do", "mi"], ["fa"]],"do"),true, "listof music notes"
   );
 
   testOperations(some([["mi", "fa", "so"], ["fa", "mi"], ["fa"]],"do"),false, "listof music notes"
   );
-
 }
-function testsOfFitnessTracker() {
-  console.log("\n--- FITNESS TRACKER MILES ---\n");
 
-  testOperations(reduce(count,[[2, 3, 2],[4],[1, 1]],0),13, "list of runner logs");
+function testsOfCount() {
+  testOperations(countOf(["red", "blue", "red", "green", "red", "blue"],"blue",0),2, "an array");
+  testOperations(countOf(["red", "red", "green", "red"],"blue", 0),0, "an array");
+  testOperations(reduce(count,[[5, 3], [1, 3, 2], [3, 2]],0,),19, "listof candies added");
+
+   testOperations(reduce(count,[[2, 3, 2],[4],[1, 1]],0),13, "list of runner logs");
 
   testOperations(reduce(count,[[1,5,0]],0), 6, "listof runner logs" );
-
+   testOperations(countOf(["dune","foundation","dune"],"dune",0),2, "list of books");
+  
+  testOperations(countOf(["story","foundation","fairy tales"],"dune",0),0, "list of books");
+  testOperations(reduce(count,[[1,2,1], [3], [2]],0,),9, "listof watering amount");
+  testOperations(reduce(count,[[3,2], [1], [4]],0,),10, "listof watering amount");
+  testOperations(countOf(["deer", "deer", "lion", "rabbit", "deer"],"deer",0),3, "an array");
+  testOperations(countOf(["rabbit", "lion", "tiger", "lion"],"deer", 0),0, "an array");
+  testOperations(reduce(count,[[4,6], [1, 3, 2], [5]],0),21, "listof weigths");
+  testOperations(reduce(count,[[2,3], [1], [2]],0),8, "listof pens handed");
+  testOperations(countOf(["chocolate", "vennila", ["chocolate"], ["vennila", "chocolate"]],
+    "chocolate",0),3, "list of ice cream flavours"
+  );
+  testOperations(reduce(count,[[10,20], [10], [15,10]],0,),65, "listof exersice count");
+  testOperations(reduce(count,[[12,10], [5], [8,7]],0,),42, "listof pages read");
 }
 
-function testsOfWeather() {
-  console.log("\n--- WEATHER VALIDATIONx ---\n");
+function testsOfFindUnique() {
+  testOperations(filter(isNotDuplicate,[["Orion", "Leo"], ["Taurus"], ["Orion", "Gemini"]]),
+    ["Orion", "Leo", "Taurus", "Gemini"], "list of names");
 
-  testOperations(every(isBelow32,[[22, 23],[25, 24, 22],[29]]),
-    true, "listof temperaues"
+  testOperations(filter(isNotDuplicate,["sparrow", "crow", "parrot", "crow"]),
+    ["sparrow", "crow", "parrot"], "list of birds"
   );
 
-  testOperations(every(isBelow32,[[22, 35],[25, 24, 22],[29]]),
-    false, "listof temperaues > 32"
+  testOperations(filter(isNotDuplicate,[["nani", "neel", "ravi", "asha"], ["neel", "meera"], ["neel"]]),
+    ["nani", "neel", "ravi", "asha", "meera"], "list of students"
   );
 
-}
 
-function testsOfColor() {
-  console.log("\n--- UNIQUE COLORS ---\n");
-
-  testOperations(filter(isUnique,[["blue", "yellow"],["yellow", "green"],["blue"]]),
+   testOperations(filter(isUnique,[["blue", "yellow"],["yellow", "green"],["blue"]]),
     ["green"], "list of paints"
   );
 
@@ -158,80 +138,43 @@ function testsOfColor() {
     ["blue","white"], "list of paints"
   );
 
-}
-
-function testsOfCountDune() {
-  console.log("\n--- COUNT DUNE---\n");
-
-  testOperations(countOf(["dune","foundation","dune"],"dune",0),2, "list of books");
-  
-  testOperations(countOf(["story","foundation","fairy tales"],"dune",0),0, "list of books");
-}
-
-function testsOfLunchBox() {
-  console.log("\n--- LUNCH BOX ---\n");
-
   testOperations(filter(isNotDuplicate,[[ "lentils", "rice"], ["curd"], ["curd","lentils"]]),
     ["lentils", "rice", "curd"], "list of ingridents"
   );
 
-}
-
-function testsOfMusicNotesOfSo() {
-  console.log("\n--- MUSIC NOTES OF SO ---\n");
-
-  testOperations(some([["mi", "fa", "so"], ["do", "mi"], ["fa"]],"so"),true, "listof music notes"
+  testOperations(filter(isNotDuplicate,[["orange","mango","banana"], ["mango", "banana"], ["apple"]]),
+    ["orange", "mango", "banana", "apple"], "list of fruits"
   );
-}
-
-function testsOfCrateTotals() {
-  console.log("\n--- CRATE TOTALS ---\n");
-
-  testOperations(reduce(count,[[4,6], [1, 3, 2], [5]],0,),21, "listof weigths");
-
-}
-
-function testsOfPostalRecords() {
-  console.log("\n--- UNIQUE POST RECORDS ---\n");
-
-  testOperations(filter(isUnique,["small","big","small","medium"]),
-    ["big","medium"], "list of records"
-  );
-}
-
-function testsOfWildLifeCount() {
-  console.log("\n--- COUNT DEERS---\n");
-
-  testOperations(countOf(["deer", "deer", "lion", "rabbit", "deer"],"deer",0),3, "an array");
-  testOperations(countOf(["rabbit", "lion", "tiger", "lion"],"deer", 0),0, "an array");
-}
-
-function testsStudyGroup() {
-  console.log("\n--- STUDY GROUP---\n");
 
   testOperations(filter(isNotDuplicate,[[1,2,3], [3], [3,4,1]]),
     [1,2,3,4], "list of chapters");
+
+    testOperations(filter(isUnique,["small","big","small","medium"]),
+    ["big","medium"], "list of records"
+  );
+
+  testOperations(filter(isNotDuplicate,["A","B","C","A","B"]),
+    ["A","B","C"], "list of signs"
+  );
+
+  testOperations(filter(isNotDuplicate,["A","B",["C","A"],["A"]]),
+    ["A","B","C"], "list of stastion names"
+  );
+
+  testOperations(filter(isNotDuplicate,[[ "rose", "lilly"], ["tulip"], ["rose","jasmine"]]),
+    ["rose", "lilly", "tulip","jasmine"], "list of flowers"
+  );
+  
 }
+
 
 
 
 function testAll() {
-  testsOfRibbonCount();
-  testsOfRemoveDuplicate();
-   testsStargazingLog();
-  testsOfAttendenceCheck();
-  testsOfCandyCount();
-  testsOfMusicNotes();
-   testsOfWeather();
-  testsOfFitnessTracker();
-   testsOfColor();
-  testsOfCountDune();
-  testsOfLunchBox();
-  testsOfMusicNotesOfSo();
-  testsOfCrateTotals();
-  testsOfPostalRecords();
-  testsOfWildLifeCount();
-  testsStudyGroup();
+ testsOfEvery();
+ testsOfSome();
+ testsOfFindUnique();
+ testsOfCount();
 }
 
 testAll();
